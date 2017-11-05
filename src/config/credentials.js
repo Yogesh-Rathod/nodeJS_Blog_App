5b6bb6b96afa4d32a7704bd7b8a47a0815b43fc9
Yogesh Rathod <yrathod101@gmail.com>
2017-11-05 20:17:25 +0530

    Config File in Git Ignore

--- src/config/credentials.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  username: "Mongo Lab Database Username",
-  password: "Mongo Lab Database Password",
-  emailUserName: 'Gmail Email Address For Node Mailer',
-  emailPassword: 'Gmail Password For Node Mailer'
-}
